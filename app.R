9bb5a892d142e8491b79a5b5c39d735fb701e7a4
Gamze Bilsen <gamzebilsen@gmail.com>
2022-04-29 20:50:58 -0400

    shiny

---
+++ app.R
@@ -0,0 +1,37 @@
+library(shiny)
+setwd("~/Documents/GitHub/Group_J_Climate_Economics/")
+
+df =  read.csv('final_to_use.csv')
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+
+country2015 <- df%>%
+  filter(Year==2015)
+
+col_choice_list = c('GDP','tmean','Population','Frequency')
+
+
+library(shiny)
+
+ui <- fluidPage(
+  selectInput("Ind","Independent Variable",choices = col_choice_list),
+  plotOutput('Hist'),
+  )
+server <- function(input, output, session) {
+  
+  data1 <- reactive({
+    input$Ind
+  })
+  
+  output$Hist <- renderPlot({
+    req(data1())
+    hist(df[[data1()]], col = "#75AADB", border = "white",
+         xlab = " ",
+         main = "Histogram ")
+  }) 
+  
+}
+
+shinyApp(ui, server)
